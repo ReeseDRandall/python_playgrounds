d33ab860741aacfd49116cd5263547e970345941
ReeseDRandall <RandallReese1121@gmail.com>
2023-02-10 13:55:29 -0800

    adding new line escape to userInput

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-userInput = input("How has your day been so far?")
 while True:
-    userInput = input("How has your day been so far?")
+    userInput = input("How has your day been so far? (good  |  okay  |  bad)\n")
 
     if userInput == "good":
         break
     elif userInput == "bad":
+        break
+    elif userInput == "okay":
         break
 
 
