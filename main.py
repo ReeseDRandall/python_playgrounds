74044044cf97641d0474225181c83891e7152aa5
ReeseDRandall <125080343+ReeseDRandall@users.noreply.github.com>
2023-02-10 11:38:32 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+userInput = input("How has your day been so far?")
+while True:
+    userInput = input("How has your day been so far?")
+
+    if userInput == "good":
+        break
+    elif userInput == "bad":
+        break
+
+
+print("")
